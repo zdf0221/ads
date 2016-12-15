6368f323b31212f9b72f8d2859e8325ce1dd025a
wuleizi <wuzqbupt@gmail.com>
2016-12-15 16:15:26 +0800

    First commit

---
+++ src/moniter/src/main/java/wu/leizi/moniter/App.java
@@ -0,0 +1,13 @@
+package wu.leizi.moniter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
